b350a63c61b0d6776333ee0d2cb86794dc3f183a
`Enrique` <`e.jimenezmoreno@gmail.com`>
2023-03-03 18:50:03 +0100

    Add server, add and connection files

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import apiRouter from './api/api-router.js';
+import authRouter from './api/auth/auth-router.js';
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.json('Server ON');
+});
+
+app.use(cors());
+app.use(express.json());
+app.disable('x-powered-by');
+
+app.use('/auth', authRouter);
+app.use('/api/v1', apiRouter);
+
+export default app;
